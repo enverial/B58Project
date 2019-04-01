572842ea8fd0d7fef2aaf22a18de30ea1295c7b2
enverial <48570691+enverial@users.noreply.github.com>
2019-04-01 18:02:29 -0400

    Update CleanupDrawings.v

--- CleanupDrawings.v
+++ CleanupDrawings.v
@@ -1,2 +1,6 @@
-module CleanupDrawings(Clock, Reset, State, XOut, YOut);
+module CharacterCleanup(Clock, Reset, State, HasMoved, XOut, YOut, Color);
   // Used to draw black where the character used to be so it doesn't leave an image
+  input Clock, Reset, HasMoved;
+  input [3:0] State;
+  output [7:0] XOut;
+  output [6:0] YOut;
